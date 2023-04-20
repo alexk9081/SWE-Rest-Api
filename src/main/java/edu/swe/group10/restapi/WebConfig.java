599025e4a72fa630be9bbd7be2844131fc2dc8ba
Alex Keo <alexk9081@gmail.com>
2023-04-20 02:16:28 -0400

    Added resource handler

--- src/main/java/edu/swe/group10/restapi/WebConfig.java
+++ src/main/java/edu/swe/group10/restapi/WebConfig.java
@@ -3,6 +3,7 @@ package edu.swe.group10.restapi;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").allowedMethods("*");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**");
   }
 }
